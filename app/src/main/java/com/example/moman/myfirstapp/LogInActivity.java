6d54d28c10082f4ebc1ce7d233b763ddce2870ce
Mohamed Yahye <moman601@hotmail.com>
2018-05-09 05:37:29 -0700

    allow user to change profile picture add crop functionality

--- app/src/main/java/com/example/moman/myfirstapp/LogInActivity.java
+++ app/src/main/java/com/example/moman/myfirstapp/LogInActivity.java
@@ -1,6 +1,8 @@
 package com.example.moman.myfirstapp;
 
 import android.content.Intent;
+import android.net.Uri;
+import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
@@ -15,6 +17,10 @@ import android.view.View;
 import android.view.Window;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
@@ -22,12 +28,32 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+import com.mikepenz.materialdrawer.AccountHeader;
+import com.mikepenz.materialdrawer.AccountHeaderBuilder;
+import com.mikepenz.materialdrawer.Drawer;
+import com.mikepenz.materialdrawer.DrawerBuilder;
+import com.mikepenz.materialdrawer.model.DividerDrawerItem;
+import com.mikepenz.materialdrawer.model.PrimaryDrawerItem;
+import com.mikepenz.materialdrawer.model.ProfileDrawerItem;
+import com.mikepenz.materialdrawer.model.SecondaryDrawerItem;
+import com.mikepenz.materialdrawer.model.interfaces.IDrawerItem;
+import com.mikepenz.materialdrawer.model.interfaces.IProfile;
+import com.squareup.picasso.Picasso;
+import com.theartofdev.edmodo.cropper.CropImage;
+import com.theartofdev.edmodo.cropper.CropImageView;
 
-public class LogInActivity extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener {
+import de.hdodenhof.circleimageview.CircleImageView;
+
+public class LogInActivity extends AppCompatActivity {
     TextView user_name;
+    CircleImageView profile_pic_view;
     private FirebaseAuth mAuth;
     private DatabaseReference databaseRef;
+    private StorageReference storeProfilePic;
+    private final static int numPictures = 1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -35,35 +61,27 @@ public class LogInActivity extends AppCompatActivity
         mAuth = FirebaseAuth.getInstance();
         String current_UID = mAuth.getCurrentUser().getUid();
         databaseRef = FirebaseDatabase.getInstance().getReference().child("Users").child(current_UID);
+        storeProfilePic = FirebaseStorage.getInstance().getReference().child("Profile_Pictures");
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        Toolbar toolbar_nav = (Toolbar) findViewById(R.id.toolbar_nav);
         setSupportActionBar(toolbar);
-
-        Toolbar toolbar_nav = (Toolbar) findViewById(R.id.toolbar_navigation);
-        setSupportActionBar(toolbar_nav);
-
-
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
-                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-        drawer.addDrawerListener(toggle);
-        toggle.syncState();
-
-        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
-        navigationView.setNavigationItemSelectedListener(this);
-
-
 
         FirebaseUser user = mAuth.getCurrentUser();
 
         user_name = (TextView) findViewById(R.id.login_username);
+        profile_pic_view = (CircleImageView) findViewById(R.id.circleImageView);
+
+        final String[] uName = new String[1];
 
         databaseRef.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
-                String uName = dataSnapshot.child("user_name").getValue().toString();
-                String profile_Pic = dataSnapshot.child("user_image").getValue().toString();
+                uName[0] = dataSnapshot.child("user_name").getValue().toString();
+                String picURL = dataSnapshot.child("user_image").getValue().toString();
 
-                user_name.setText(uName);
+                user_name.setText(uName[0]);
+                Picasso.get().load(picURL).into(profile_pic_view);
             }
 
             @Override
@@ -71,14 +89,53 @@ public class LogInActivity extends AppCompatActivity
 
             }
         });
+        //-------------------- Account header ---------------------------//
+        // Create the AccountHeader
+        AccountHeader headerResult = new AccountHeaderBuilder()
+                .withActivity(this)
+                .withHeaderBackground(R.drawable.header)
+                .addProfiles(
+                        new ProfileDrawerItem().withName(uName[0]).withEmail("mikepenz@gmail.com")
+                )
+                .withOnAccountHeaderListener(new AccountHeader.OnAccountHeaderListener() {
+                    @Override
+                    public boolean onProfileChanged(View view, IProfile profile, boolean currentProfile) {
+                        return false;
+                    }
+                })
+                .build();
+
+        //-------------------- Navigation Drawer ------------------------//
+        //if you want to update the items at a later time it is recommended to keep it in a variable
+        PrimaryDrawerItem item1 = new PrimaryDrawerItem().withIdentifier(1).withName("Home");
+        PrimaryDrawerItem item2 = new PrimaryDrawerItem().withIdentifier(2).withName("Friends");
+
+        //create the drawer and remember the `Drawer` result object
+        Drawer result = new DrawerBuilder()
+                .withActivity(this)
+                .withAccountHeader(headerResult)
+                .withToolbar(toolbar_nav)
+                .addDrawerItems(
+                        item1,item2
+                )
+                .withOnDrawerItemClickListener(new Drawer.OnDrawerItemClickListener() {
+                    @Override
+                    public boolean onItemClick(View view, int position, IDrawerItem drawerItem) {
+                        // do something with the clicked item :D
+                        switch (position){
+                            case 1: break;
+                            case 2: break;
+                        }
+                        return true;
+                    }
+                })
+                .build();
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.login_menu, menu);
-        inflater.inflate(R.menu.navigation, menu);
-
         return super.onCreateOptionsMenu(menu);
     }
 
@@ -111,41 +168,80 @@ public class LogInActivity extends AppCompatActivity
     }
 
     public void changeProfilePicture(View view) {
-
+        Intent newGalleryIntent = new Intent();
+        newGalleryIntent.setAction(Intent.ACTION_GET_CONTENT);
+        newGalleryIntent.setType("image/*");
+        startActivityForResult(newGalleryIntent, numPictures);
     }
 
     @Override
-    public void onBackPressed() {
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        if (drawer.isDrawerOpen(GravityCompat.START)) {
-            drawer.closeDrawer(GravityCompat.START);
-        } else {
-            super.onBackPressed();
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode == numPictures && resultCode == RESULT_OK && data != null){
+            Uri imageURI = data.getData();
+            CropImage.activity()
+                    .setGuidelines(CropImageView.Guidelines.ON)
+                    .setAspectRatio(1,1)
+                    .start(this);
+        }
+
+        if (requestCode == CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE) {
+            CropImage.ActivityResult result = CropImage.getActivityResult(data);
+            if (resultCode == RESULT_OK) {
+                Uri resultUri = result.getUri();
+                String uID = mAuth.getCurrentUser().getUid();
+                StorageReference filepath = storeProfilePic.child(uID + ".jpg");
+                filepath.putFile(resultUri).addOnCompleteListener(new OnCompleteListener<UploadTask.TaskSnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<UploadTask.TaskSnapshot> task) {
+                        if(task.isSuccessful()){
+                            Toast.makeText(getApplicationContext(), "Uploaded image to firebase storage..", Toast.LENGTH_SHORT).show();
+                            String downloadURL = task.getResult().getDownloadUrl().toString();
+                            databaseRef.child("user_image").setValue(downloadURL);
+                        } else {
+                            Toast.makeText(getApplicationContext(), "Error occurred while uploading image to storage..", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+            } else if (resultCode == CropImage.CROP_IMAGE_ACTIVITY_RESULT_ERROR_CODE) {
+                Exception error = result.getError();
+            }
         }
     }
 
-    @SuppressWarnings("StatementWithEmptyBody")
-    @Override
-    public boolean onNavigationItemSelected(MenuItem item) {
-        // Handle navigation view item clicks here.
-        int id = item.getItemId();
+    //    @Override
+//    public void onBackPressed() {
+//        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+//        if (drawer.isDrawerOpen(GravityCompat.START)) {
+//            drawer.closeDrawer(GravityCompat.START);
+//        } else {
+//            super.onBackPressed();
+//        }
+//    }
 
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
-
-        } else if (id == R.id.nav_slideshow) {
-
-        } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
-
-        }
-
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        drawer.closeDrawer(GravityCompat.START);
-        return true;
-    }
+//    @SuppressWarnings("StatementWithEmptyBody")
+//    @Override
+//    public boolean onNavigationItemSelected(MenuItem item) {
+//        // Handle navigation view item clicks here.
+//        int id = item.getItemId();
+//
+//        if (id == R.id.nav_camera) {
+//            // Handle the camera action
+//        } else if (id == R.id.nav_gallery) {
+//
+//        } else if (id == R.id.nav_slideshow) {
+//
+//        } else if (id == R.id.nav_manage) {
+//
+//        } else if (id == R.id.nav_share) {
+//
+//        } else if (id == R.id.nav_send) {
+//
+//        }
+//
+//        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+//        drawer.closeDrawer(GravityCompat.START);
+//        return true;
+//    }
 }
